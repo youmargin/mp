3675fb7debb7661094c7127ce7272b1c4733f0d5
ccj <ccjsmile@163.com>
2020-04-28 15:29:58 +0800

    增加日志模块，insert测试

--- src/test/java/com/app/mp/MpApplicationTests.java
+++ src/test/java/com/app/mp/MpApplicationTests.java
@@ -6,16 +6,19 @@ import com.app.mp.entity.User;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MpApplicationTests {
-
+    private static Logger logger = LoggerFactory.getLogger(MpApplicationTests.class);
     @Autowired
     private UserMapper userMapper;
 
@@ -23,6 +26,19 @@ public class MpApplicationTests {
     public void selectTest(){
         List<User> userList = userMapper.selectList(null);
         Assert.assertEquals(5, userList.size());
-        System.out.println(111);
+        for(User user : userList){
+            logger.debug("{}",user);
+        }
+    }
+
+    @Test
+    public void insertTest(){
+        User user = new User();
+        user.setName("郝红梅");
+        user.setAge(30);
+        user.setManagerId(1088248166370832385l);
+        user.setCreateTime(LocalDateTime.now());
+        int rows = userMapper.insert(user);
+        logger.debug("rows = {}", rows);
     }
 }
