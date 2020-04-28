9a53072c60c510d2d62db408e24f39f96d127dd8
ccj <ccjsmile@163.com>
2020-04-28 15:17:00 +0800

    使用lombok

--- src/main/java/com/app/mp/entity/User.java
+++ src/main/java/com/app/mp/entity/User.java
@@ -1,7 +1,10 @@
 package com.app.mp.entity;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class User {
     private Long id;
     private String name;
@@ -10,51 +13,5 @@ public class User {
     private Long managerId;
     private LocalDateTime createTime;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getManagerId() {
-        return managerId;
-    }
-
-    public void setManagerId(Long managerId) {
-        this.managerId = managerId;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
 }
